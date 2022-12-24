810a45c2c2119262351baa67a2cf18d80f5e1213
vandeurenglenn <vandeurenglenn@gmail.com>
2022-12-24 20:38:03 +0100

    Update http.js

--- exports/http.js
+++ exports/http.js
@@ -30,6 +30,26 @@ export default (chain, port, networkVersion) => {
     return
   })
 
+  router.get('/deployContract', async ctx => {
+    try {
+      const tx = await chain.deployContract(ctx.query.code, ctx.query.params)
+      await tx.wait()
+      delete tx.wait
+      ctx.body = tx
+    } catch (error) {
+      
+    }
+  })
+  
+  router.get('/createContractAddress', async ctx => {
+    try {
+      const address = await chain.createContractAddress(ctx.query.owner, ctx.query.code, ctx.query.params)
+      ctx.body = address
+    } catch (error) {
+      
+    }
+  })
+
   // todo finish ...
   api.use(cors('*'))
   api.use(router.routes())
