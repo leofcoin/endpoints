357639cac86f2277a1e0cb6b5d727889be7ba59e
vandeurenglenn <vandeurenglenn@gmail.com>
2022-12-25 14:29:37 +0100

    http: finish api

--- exports/http.js
+++ exports/http.js
@@ -30,12 +30,95 @@ export default (chain, port, networkVersion) => {
     return
   })
 
+  router.get('/accounts', async ctx => {
+    ctx.body = JSON.parse(new TextDecoder().decode((await walletStore.get('accounts'))))
+  })
+
+  router.get('/hasTransactionToHandle', async ctx => {
+    ctx.body = await chain.hasTransactionToHandle()
+  })
+  router.get('/getBlock', ctx => ctx.body = chain.blocks[ctx.query.index])
+
+  router.get('/blocks', ctx => {
+    ctx.body = chain.blocks.slice(ctx.query.amount)
+  })
+
+  router.get('/createTransactionFrom', async ctx => {
+    try {
+      const tx = await chain.createTransactionFrom(ctx.query.from, ctx.query.to, ctx.query.method, ctx.query.parameters, ctx.query.nonce)
+      await tx.wait()
+      send(tx)
+    } catch (error) {
+      send(202)
+    }
+  })
+
+  router.get('/peerId', ctx => 
+    ctx.body = peernet.peerId
+  )
+
+  router.get('/validators', ctx => 
+    ctx.body = chain.validators
+  )
+
+  router.get('/lookup', ctx => 
+    ctx.body = chain.lookup(ctx.query.name)
+  )
+  
+  router.get('/staticCall', async ctx => 
+    ctx.body = await chain.staticCall(ctx.query.contract, ctx.query.method, ctx.query.params)
+  )
+  
+  router.get('/nativeBurns', ctx => 
+    ctx.body = chain.nativeBurns
+  )
+
+  router.get('/contracts', ctx => 
+    ctx.body = chain.contracts
+  )
+  
+  router.get('/nativeMints', ctx => 
+    ctx.body = chain.nativeMints
+  )
+
+  router.get('/nativeToken', ctx => 
+    ctx.body = chain.nativeToken
+  )
+
+  router.get('/nativeTransfers', ctx =>
+    ctx.body = chain.nativeTransfers
+  )
+
+  router.get('/totalSize', ctx => 
+    ctx.body = chain.totalSize
+  )
+    
+  router.get('/totalTransactions', ctx => 
+    ctx.body = chain.totalTransactions
+  )
+
+  router.get('/totalBlocks', ctx => 
+    ctx.body = chain.blocks.length
+  )  
+    
+  router.get('/nativeCalls', ctx => 
+    ctx.body = chain.nativeCalls
+  )
+  
+  router.get('/participating', ctx =>
+    ctx.body = chain.participating
+  )
+
+  router.get('participate', async ctx =>
+    ctx.body = await chain.participate(ctx.query.address)
+  )
+
   router.get('/deployContract', async ctx => {
     try {
       const tx = await chain.deployContract(ctx.query.code, ctx.query.params)
       await tx.wait()
       delete tx.wait
-      ctx.body = tx
+      ctx.body = JSON.stringify(tx)
     } catch (error) {
       
     }
