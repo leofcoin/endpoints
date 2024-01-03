fa03c6bb30466ff9b940fcd68ba73764530edb02
VandeurenGlenn <vandeurenglenn@gmail.com>
2024-01-03 20:07:07 +0100

    cleanup repo: remove exports

--- exports/http.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- *
- * ensure remote is set to false when serving on a server
- */
-declare const _default: (chain: any, port: any, networkVersion: any, remote?: boolean) => void;
-export default _default;
--- exports/ws.d.ts
+++
@@ -1,5 +0,0 @@
-declare const _default: (chain: any, port: any, networkVersion: any, remote?: boolean) => Promise<{
-    close: () => any;
-    connections: any[];
-}>;
-export default _default;
